a3daff882629f7e687d1d7e23142be8b7555a03e
Mike Wright <mwright@pivotal.io>
2017-01-30 17:04:00 -0500

    fix

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,7 +5,7 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd df-repo/${SUBPROJECT}
 echo $version
-./mvnw clean package -Djar.finalName=${SUBPROJECT}_${version}.jar
+./mvnw clean package -Djar.finalName=${SUBPROJECT}_${version}
 pwd
 ls target/.
 cp target/${SUBPROJECT}_${version}.jar ../../build-output/.
