ae8e1ef87bdb90cd56bc180a42e1aa9f8423c285
Mike Wright <mwright@gopivotal.com>
2017-01-30 08:53:14 -0500

    Update run-local.ps1
    add quotes

--- ci/run-local.ps1
+++ ci/run-local.ps1
@@ -1,9 +1,9 @@
 $client = New-Object System.Net.WebClient
-$client.DownloadFile(http://repo.spring.io/snapshot/org/springframework/cloud/spring-cloud-dataflow-server-local/1.2.0.BUILD-SNAPSHOT/spring-cloud-dataflow-server-local-1.2.0.BUILD-SNAPSHOT.jar, df.jar)
+$client.DownloadFile("http://repo.spring.io/snapshot/org/springframework/cloud/spring-cloud-dataflow-server-local/1.2.0.BUILD-SNAPSHOT/spring-cloud-dataflow-server-local-1.2.0.BUILD-SNAPSHOT.jar", "c:\temp\df.jar")
 
-$client.DownloadFile(http://repo.spring.io/snapshot/org/springframework/cloud/spring-cloud-dataflow-shell/1.2.0.BUILD-SNAPSHOT/spring-cloud-dataflow-shell-1.2.0.BUILD-SNAPSHOT.jar, dfshell.jar)
+$client.DownloadFile("http://repo.spring.io/snapshot/org/springframework/cloud/spring-cloud-dataflow-shell/1.2.0.BUILD-SNAPSHOT/spring-cloud-dataflow-shell-1.2.0.BUILD-SNAPSHOT.jar", "c:tmep\dfshell.jar")
 
 $env:JAVA_HOME = "C:\Program Files\Java\jdk1.8.0_121"
 $env:PATH += $env:JAVA_HOME+"\bin"
 
-Start-Process -FilePath java.exe -ArgumentList '-jar','df.jar','-classpath','$Env:CLASSPATH' -NoNewWindow
+Start-Process -FilePath java.exe -ArgumentList '-jar','c:\temp\df.jar','-classpath','$Env:CLASSPATH' -NoNewWindow
