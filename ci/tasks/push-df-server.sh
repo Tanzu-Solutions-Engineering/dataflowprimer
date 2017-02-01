eb66d46529f495d520ee3f911e583a5c8bc93dda
Mike Wright <mwright@pivotal.io>
2017-02-01 15:23:24 -0500

    fix df push

--- ci/tasks/push-df-server.sh
+++ ci/tasks/push-df-server.sh
@@ -8,10 +8,10 @@ cf login -u ${USERNAME} -p ${PASSWORD} -o ${ORG} -s ${SPACE} -a ${API_ENDPOINT} 
 cf delete dataflow-server -r -f
 
 cf delete-service df-mysql -f
-cf create-service p-mysql 512mb df-mysql
+cf create-service p-mysql 10mb df-mysql
  
 #custom built SCDF server to enable PCF features
-cf push dataflow-server -n df-mwright -m 2G -k 2G -b java_buildpack -p spring-cloud-dataflow-server-cloudfoundry-1.2.0.BUILD-SNAPSHOT.jar --no-start
+cf push dataflow-server -n df-mwright -m 2G -k 2G -b java_buildpack_offline -p spring-cloud-dataflow-server-cloudfoundry-1.2.0.BUILD-SNAPSHOT.jar --no-start
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_URL ${API_ENDPOINT}
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_ORG ${ORG}
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SPACE ${SPACE}
@@ -22,7 +22,7 @@ cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_PASSWORD ${PASSWOR
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION true
 cf set-env dataflow-server MAVEN_REMOTE_REPOSITORIES_REPO1_URL https://repo.spring.io/libs-snapshot
 cf set-env dataflow-server MAVEN_REMOTE_REPOSITORIES_REPO2_URL https://repo.spring.io/libs-milestone
-cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_BUILDPACK java_buildpack
+cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_BUILDPACK java_buildpack_offline
 cf set-env dataflow-server SPRING_CLOUD_DATAFLOW_FEATURES_EXPERIMENTAL_TASKSENABLED true
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_SERVICES df-mysql,df-rabbit
 cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_API_TIMEOUT 500
